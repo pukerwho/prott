a6fcbcdd37ecd46bb9bbbf41df097daf0b077936
Puker Who <nawwesele@gmail.com>
2024-08-04 19:02:44 +0300

    edit

--- inc/tasks/task-link.php
+++ inc/tasks/task-link.php
@@ -12,11 +12,27 @@ function task_link_function() {
     add_post_meta( $post_id, '_crb_tasks_post_link', $task_link, true ); 
     update_post_meta( $post_id, '_crb_tasks_status', 'На перевірці' );
   } 
+  sendTelegramAuthor($task_id, $task_link);
   echo $post_id;
   wp_die();
 }
 
 add_action('wp_ajax_task_link_click_action', 'task_link_function');
 add_action('wp_ajax_nopriv_task_link_click_action', 'task_link_function');
+
+function sendTelegramLink($id, $task_link) {
+  $chatID = carbon_get_theme_option("crb_telegram_chat_id");
+  $apiToken = carbon_get_theme_option("crb_telegram_api");
+  $content = "";
+  $content .= "Угода <b>$id</b> виконана. </b>.\n\n";
+  $content .= "<b>Посилання:</b> task_link</b>";
+  
+  $data = [
+    'chat_id' => $chatID, 
+    'text' => $content,
+    'parse_mode' => 'HTML'
+  ];
+  $response = file_get_contents("https://api.telegram.org/bot".$apiToken."/sendMessage?" . http_build_query($data) );
+}
 
 ?>
